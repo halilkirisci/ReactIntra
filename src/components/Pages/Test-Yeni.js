dd7cd63d16d401010ca7d0ca94453c0ab1c6f428
Halil Kirişci <halilkirisci@gmail.com>
2018-07-09 15:57:40 +0300

    zxc

--- src/components/Pages/Test-Yeni.js
+++ src/components/Pages/Test-Yeni.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Datetime from 'react-datetime';
 import { Link } from 'react-router-dom';
 import $ from 'jquery';
 import Moment from 'moment';
@@ -11,11 +12,13 @@ class TestYeni extends Component {
   render() {
     return (
       <div>
+        <Datetime locale="tr" dateFormat="DD.MM.YYYY" timeFormat={false} />
         <h2>Yeni Test Giriş</h2>
         <form className="form-horizontal">
           <div className="form-group">
             <input type="text" className="form-control" />
           </div>
+
           <input type="submit" className="btn btn-default" value="Gönder" />
         </form>
       </div>
