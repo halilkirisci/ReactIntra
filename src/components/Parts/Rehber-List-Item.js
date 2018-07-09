42d56251d50c398a0323a9d78cf4fa0dfe7b2724
Erhan Güney Karadağ <guneykaradag@hotmail.com>
2018-07-09 15:19:52 +0300

    son push

--- src/components/Parts/Rehber-List-Item.js
+++ src/components/Parts/Rehber-List-Item.js
@@ -6,19 +6,27 @@ const RehberListItem = props => {
   const { kisi } = props;
 
   return (
-    <div className="card">
-      <div className="card-body text-center">
-        <p>
-          <Vesikalik key={kisi.KOD} kod={kisi.KOD} />
-        </p>
-        <h4 className="card-title">
-          {kisi.PERSONEL_AD} {kisi.SOYAD}
-        </h4>
-        <h5 className="card-text">{kisi.FİİLİ_GÖREV_YERİ}</h5>
-        <h6 className="card-text">{kisi.İş_Tel}</h6>
-        <a href="" className="btn btn-primary btn-sm">
-          <i className="fa fa-plus" />
-        </a>
+    <div className="m-portlet ">
+      <div className="m-portlet__body">
+        <div className="m-card-profile">
+          <div className="m-card-profile__title m--hide">Your Profile</div>
+          <div className="m-card-profile__pic">
+            <div className="m-card-profile__pic-wrapper">
+              <Vesikalik key={kisi.KOD} kod={kisi.KOD} />
+            </div>
+          </div>
+          <div className="m-card-profile__details">
+            <span className="m-card-profile__name">
+              {kisi.PERSONEL_AD} {kisi.SOYAD}
+            </span>
+            <a href="" className="m-card-profile__email m-link">
+              {kisi.FİİLİ_GÖREV_YERİ}
+              <br /> {kisi.İş_Tel}
+            </a>
+          </div>
+        </div>
+
+        <div className="m-portlet__body-separator" />
       </div>
     </div>
   );
