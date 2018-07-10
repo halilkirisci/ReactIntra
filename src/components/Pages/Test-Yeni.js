0a8c12ab3e95247752b29bc6e8f4d133f05d8e89
Halil Kirişci <halilkirisci@gmail.com>
2018-07-10 15:36:44 +0300

    s

--- src/components/Pages/Test-Yeni.js
+++ src/components/Pages/Test-Yeni.js
@@ -7,8 +7,10 @@ import 'react-datepicker/dist/react-datepicker.css';
 class TestYeni extends Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    const dd = new Date();
+    this.state = { startDate: dd.now };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(date) {
@@ -16,19 +18,46 @@ class TestYeni extends Component {
       startDate: date,
     });
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    //console.log(data);
+    /*for (let name of data.keys()) {
+      console.log(name + ':' + data.get(name));
+    }*/
+
+    fetch('http://172.17.4.29/intapi/api/W_YEMEK', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        ID: 99,
+        YEMEK_ADI: data.get('adi'),
+        KALORI: data.get('kalori'),
+        TARIH: data.get('tarih'),
+      }),
+    }).catch(error => console.log('hata', error));
+    // console.log(data);
+  }
   render() {
     return (
       <div>
-        <DatePicker
-          className="form-control"
-          selected={this.state.startDate}
-          onChange={this.handleChange}
-        />
-        <form className="form-horizontal">
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <input type="text" className="form-control" />
+            <DatePicker
+              name="tarih"
+              className="form-control"
+              dateFormat="DD.MM.YYYY"
+              selected={this.state.startDate}
+              onChange={this.handleChange}
+            />
           </div>
-
+          <div className="form-group">
+            <input name="adi" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <input name="kalori" type="text" className="form-control" />
+          </div>
           <input type="submit" className="btn btn-default" value="Gönder" />
         </form>
       </div>
